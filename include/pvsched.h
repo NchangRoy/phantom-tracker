e4beb0f2497b194398c7a2fdfe0602a4291a3ec4
Himadri Chhaya-Shailesh <himadrics@protonmail.com>
2026-05-10 23:19:36 +0200

    pvsched: Add struct hg_message definition

---
+++ include/pvsched.h
@@ -0,0 +1,44 @@
+/*
+ * SPDX-FileCopyrightText: 2021-2025 Inria
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * Contributors:
+ *   Humans: Himadri Chhaya-Shailesh, Jean-Pierre Lozi, Julia Lawall
+ *   AI: Claude Sonnet 4.6, ChatGPT-5.5
+ */
+
+#ifndef _PVSCHED_H
+#define _PVSCHED_H
+
+/* Kbuild should define this for the pvsched-shmem kernel modules */
+#ifdef __KERNEL__
+
+#include <linux/types.h>
+typedef u64 pvsched_u64;
+
+/* 
+ * GCC / Clang should define this for the eBPF programs.
+ * eBPF programs should include vmlinux.h first. 
+ */
+#elif defined(__BPF__)
+
+typedef __UINT64_TYPE__ pvsched_u64;
+
+/* Userspace libgomp case */
+#else
+
+#include <stdint.h>
+typedef uint64_t pvsched_u64;
+
+#endif /* Kernel || eBPF || Userspace */
+
+/* 
+ * Best effort cache-line-alignment using padding because
+ * __attribute__((aligned(...))) is not allowed in eBPF.
+ */
+struct hg_message {
+	pvsched_u64 msg;
+	pvsched_u64 padding [7];
+};
+
+#endif /* _PVSCHED_H */
