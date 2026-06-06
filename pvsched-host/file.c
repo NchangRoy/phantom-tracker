3a5c0130826f5d781962e80a5d7b1c57abae37dd
root <root@gros-17.nancy.grid5000.fr>
2026-06-06 23:05:22 +0200

    file.c

---
+++ pvsched-host/file.c
@@ -0,0 +1,228 @@
+
+          <idle>-0       [030] ..s2.  2585.596607: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596607: bpf_trace_printk: duration :107796030
+
+          <idle>-0       [030] ..s2.  2585.596607: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596607: bpf_trace_printk: duration :125951
+
+          <idle>-0       [030] ..s2.  2585.596607: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596608: bpf_trace_printk: duration :3882182
+
+          <idle>-0       [030] ..s2.  2585.596608: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596608: bpf_trace_printk: duration :26515
+
+          <idle>-0       [030] ..s2.  2585.596608: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596608: bpf_trace_printk: duration :63982799
+
+          <idle>-0       [030] ..s2.  2585.596608: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596609: bpf_trace_printk: duration :78657
+
+          <idle>-0       [030] ..s2.  2585.596609: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596609: bpf_trace_printk: duration :33686
+
+          <idle>-0       [030] ..s2.  2585.596609: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596610: bpf_trace_printk: duration :67554
+
+          <idle>-0       [030] ..s2.  2585.596610: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596610: bpf_trace_printk: duration :3806470
+
+          <idle>-0       [030] ..s2.  2585.596610: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596611: bpf_trace_printk: duration :73114
+
+          <idle>-0       [030] ..s2.  2585.596611: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596611: bpf_trace_printk: duration :27922799
+
+          <idle>-0       [030] ..s2.  2585.596611: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596611: bpf_trace_printk: duration :211208
+
+          <idle>-0       [030] ..s2.  2585.596612: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596612: bpf_trace_printk: duration :24361409
+
+          <idle>-0       [030] ..s2.  2585.596612: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596612: bpf_trace_printk: duration :332685
+
+          <idle>-0       [030] ..s2.  2585.596612: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596613: bpf_trace_printk: duration :3161
+
+          <idle>-0       [030] ..s2.  2585.596613: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596613: bpf_trace_printk: duration :176178
+
+          <idle>-0       [030] ..s2.  2585.596613: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596614: bpf_trace_printk: duration :27247
+
+          <idle>-0       [030] ..s2.  2585.596614: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596614: bpf_trace_printk: duration :84484
+
+          <idle>-0       [030] ..s2.  2585.596614: bpf_trace_printk: count :23
+
+          <idle>-0       [030] ..s2.  2585.596614: bpf_trace_printk: duration :17038
+
+          <idle>-0       [030] ..s2.  2585.596614: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596615: bpf_trace_printk: duration :28711
+
+          <idle>-0       [030] ..s2.  2585.596615: bpf_trace_printk: count :23
+
+          <idle>-0       [030] ..s2.  2585.596615: bpf_trace_printk: duration :14339
+
+          <idle>-0       [030] ..s2.  2585.596615: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596615: bpf_trace_printk: duration :38982
+
+          <idle>-0       [030] ..s2.  2585.596616: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596616: bpf_trace_printk: duration :2710703
+
+          <idle>-0       [030] ..s2.  2585.596616: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596616: bpf_trace_printk: duration :1387
+
+          <idle>-0       [030] ..s2.  2585.596616: bpf_trace_printk: count :22
+
+          <idle>-0       [030] ..s2.  2585.596617: bpf_trace_printk: duration :-1900006650
+
+          <idle>-0       [030] ..s2.  2585.596617: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596617: bpf_trace_printk: duration :1900079293
+
+          <idle>-0       [030] ..s2.  2585.596617: bpf_trace_printk: count :23
+
+          <idle>-0       [030] ..s2.  2585.596617: bpf_trace_printk: duration :1443
+
+          <idle>-0       [030] ..s2.  2585.596618: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596618: bpf_trace_printk: duration :1414
+
+          <idle>-0       [030] ..s2.  2585.596618: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596618: bpf_trace_printk: duration :75912635
+
+          <idle>-0       [030] ..s2.  2585.596618: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596619: bpf_trace_printk: duration :122411
+
+          <idle>-0       [030] ..s2.  2585.596619: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596619: bpf_trace_printk: duration :3870636
+
+          <idle>-0       [030] ..s2.  2585.596619: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596619: bpf_trace_printk: duration :61245
+
+          <idle>-0       [030] ..s2.  2585.596620: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596620: bpf_trace_printk: duration :99937885
+
+          <idle>-0       [030] ..s2.  2585.596620: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596620: bpf_trace_printk: duration :207201
+
+          <idle>-0       [030] ..s2.  2585.596620: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596621: bpf_trace_printk: duration :79805013
+
+          <idle>-0       [030] ..s2.  2585.596621: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596621: bpf_trace_printk: duration :131527
+
+          <idle>-0       [030] ..s2.  2585.596621: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596621: bpf_trace_printk: duration :3855102
+
+          <idle>-0       [030] ..s2.  2585.596621: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596622: bpf_trace_printk: duration :63755
+
+          <idle>-0       [030] ..s2.  2585.596622: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596622: bpf_trace_printk: duration :15944913
+
+          <idle>-0       [030] ..s2.  2585.596622: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596622: bpf_trace_printk: duration :106182
+
+          <idle>-0       [030] ..s2.  2585.596623: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596623: bpf_trace_printk: duration :3884322
+
+          <idle>-0       [030] ..s2.  2585.596623: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596623: bpf_trace_printk: duration :61136
+
+          <idle>-0       [030] ..s2.  2585.596623: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596624: bpf_trace_printk: duration :103954587
+
+          <idle>-0       [030] ..s2.  2585.596624: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596624: bpf_trace_printk: duration :164394
+
+          <idle>-0       [030] ..s2.  2585.596624: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596624: bpf_trace_printk: duration :63843032
+
+          <idle>-0       [030] ..s2.  2585.596625: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596625: bpf_trace_printk: duration :84952
+
+          <idle>-0       [030] ..s2.  2585.596625: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596625: bpf_trace_printk: duration :27475
+
+          <idle>-0       [030] ..s2.  2585.596625: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596625: bpf_trace_printk: duration :60888
+
+          <idle>-0       [030] ..s2.  2585.596626: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596626: bpf_trace_printk: duration :3812364
+
+          <idle>-0       [030] ..s2.  2585.596626: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596626: bpf_trace_printk: duration :73030
+
+          <idle>-0       [030] ..s2.  2585.596626: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596627: bpf_trace_printk: duration :27922551
+
+          <idle>-0       [030] ..s2.  2585.596627: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596627: bpf_trace_printk: duration :116049
+
+          <idle>-0       [030] ..s2.  2585.596627: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596627: bpf_trace_printk: duration :3883040
+
+          <idle>-0       [030] ..s2.  2585.596628: bpf_trace_printk: count :24
+
+          <idle>-0       [030] ..s2.  2585.596628: bpf_trace_printk: duration :66818
+
+          <idle>-0       [030] ..s2.  2585.596628: bpf_trace_printk: count :25
+
+          <idle>-0       [030] ..s2.  2585.596628: bpf_trace_printk: duration :91071737
+
+          <idle>-0       [030] ..s2.  2585.600684: bpf_trace_printk: Sum 30563360874
+
+          <idle>-0       [030] ..s2.  2585.600685: bpf_trace_printk: total_time 1071310377
+
+          <idle>-0       [030] ..s2.  2585.600685: bpf_trace_printk: Phantom average is 28
+
+^C
