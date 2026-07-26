f1dccbdc0dc3d18ddac9cca7dff606db0db69abb
NchangRoy <royfru44@gmail.com>
2026-07-26 18:20:15 +0100

    phantom-tracker:fixed compilation errors on host ivshmem driver

--- pvsched-shmem/host/host_ivshmem.c
+++ pvsched-shmem/host/host_ivshmem.c
@@ -185,8 +185,8 @@ PVSCHED_KFUNC_DEFS_END();
 
 
 PVSCHED_KFUNCS_START(bpf_host_ivshmem_kfuncs)
-BTF_ID_FLAGS(func, bpf_host_ivshmem_h2g_write)
-BTF_ID_FLAGS(func, bpf_host_ivshmem_g2h_read)
+BTF_ID_FLAGS(func, bpf_host_ivshmem_h2g_write, KF_TRUSTED_ARGS)
+BTF_ID_FLAGS(func, bpf_host_ivshmem_g2h_read, KF_TRUSTED_ARGS)
 PVSCHED_KFUNCS_END(bpf_host_ivshmem_kfuncs)
 
 static const struct btf_kfunc_id_set bpf_host_ivshmem_kfunc_id_set = {
@@ -195,11 +195,11 @@ static const struct btf_kfunc_id_set bpf_host_ivshmem_kfunc_id_set = {
 };
 
 /*
-Registering the kfunc for all BPF program types
+Registering the kfunc for KPROBE BPF program type
 */
 static int host_ivshmem_register_kfuncs(void)
 {
-	return register_btf_kfunc_id_set(BPF_PROG_TYPE_UNSPEC,
+	return register_btf_kfunc_id_set(BPF_PROG_TYPE_KPROBE,
 					  &bpf_host_ivshmem_kfunc_id_set);
 }
 
